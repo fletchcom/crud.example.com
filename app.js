452e6fffa5fcbeda4986ce72dcf07395c2dc166a
laurafletcher <laura@fletchcom.net>
2020-11-05 21:58:22 -0600

    Connect to the database

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,6 +11,9 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 var config = require('./config.dev');
+
+//Connect to MongoDB
+mongoose.connect(config.mongodb, { useNewUrlParser: true });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
