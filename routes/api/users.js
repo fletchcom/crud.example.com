9a755aeae788ea75017fce2f9c2b1e8ec054e027
laurafletcher <laura@fletchcom.net>
2020-11-05 23:38:55 -0600

    Correct errors - create user

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,6 +11,7 @@ router.get('/', function(req, res, next) {
   });
 });
 
+
 router.get('/:userId', function(req,res){
   
     var userId = req.params.userId;
@@ -22,6 +23,16 @@ router.get('/:userId', function(req,res){
      });
    });
 
+   router.get('/:userId', function(req,res){
+
+    var userId = req.params.userId;
+     Users.findOne({'_id':userId}, function(err, user){
+       if(err){
+        return res.json({'success':false, 'error': err});
+      }
+       return res.json({'success':true, 'user': user});
+     });
+   });
    router.post('/', function(req, res) {
     Users.create(new Users({
       username: req.body.username,
@@ -29,44 +40,43 @@ router.get('/:userId', function(req,res){
       first_name: req.body.first_name,
       last_name: req.body.last_name
     }), function(err, user){
-      
+
       if(err){
         return res.json({success: false, user: req.body, error: err});
       }
-  
-      return res.json({success: true, user: user});
-      
-    });
-  }); 
 
+      return res.json({success: true, user: user});
+
+    });
+  });
   router.put('/', function(req, res){
 
     Users.findOne({'_id': req.body._id}, function(err, user){
-  
+
      if(err) {
        return res.json({success: false, error: err});
      }
-  
+
      if(user) {
-  
+
       let data = req.body;
-  
+
       if(data.username){
         user.username = data.username;
       };
-  
+
       if(data.email){
       user.email = data.email;
       };
-  
+
       if(data.first_name){
       user.first_name = data.first_name;
       };
-  
+
       if(data.last_name){
       user.last_name = data.last_name;
       };
-  
+
       user.save(function(err){
         if(err){
           return res.json({success: false, error: err});
@@ -74,27 +84,27 @@ router.get('/:userId', function(req,res){
           return res.json({success: true, user:user});
         }
       });
-  
+
      }
-  
+
     });
-    
+
   });
 
   router.delete('/:userId', function(req,res){
 
     var userId = req.params.userId;
-  
+
     Users.remove({'_id':userId}, function(err,removed){
-  
+
       if(err){
         return res.json({success: false, error: err});
       }
-  
-      return res.json({success: true, status: removed});
-  
-    });
-  
-  });
 
+      return res.json({success: true, status: removed});
+
+    });
+
+  });
+  
 module.exports = router;
